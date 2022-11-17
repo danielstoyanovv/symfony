ac50ebdaadbac703ce32e22d6b4275391116e731
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2022-11-17 18:00:54 +0200

    Remove wrong added queries in migration

--- migrations/Version20221113183924.php
+++ migrations/Version20221113183924.php
@@ -29,7 +29,5 @@ final class Version20221113183924 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE rating_data DROP FOREIGN KEY FK_698B60E6A0BDB2F3');
         $this->addSql('DROP INDEX IDX_698B60E6A0BDB2F3 ON rating_data');
-        $this->addSql('ALTER TABLE rating_data CHANGE customer_name customer_name VARCHAR(256) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE customer_email customer_email VARCHAR(256) NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE song CHANGE name name VARCHAR(256) NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
