9eb6b6f090c12df516218fd01f31eca4e89b70e4
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2022-11-30 10:45:26 +0200

    Add PaypalInterface

---
+++ src/Service/PaypalInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Service;
+
+interface PaypalInterface
+{
+    /**
+     * @param string $orderId
+     * @param string $paypalApiUrl
+     * @param string $token
+     * @return void
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function capture(string $orderId, string $paypalApiUrl, string $token): void;
+
+    /**
+     * @param string $paypalApiUrl
+     * @param string $paypaAuthorizationCode
+     * @return mixed|string
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function getToken(string $paypalApiUrl, string $paypaAuthorizationCode);
+
+    /**
+     * @param string $orderId
+     * @param string $paypalUrl
+     * @return string
+     */
+    public function getCheckoutNowUrl(string $orderId, string $paypalUrl): string;
+
+    /**
+     * @param string $token
+     * @param int $amount
+     * @param string $paypalApiUrl
+     * @return array|mixed
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function createOrder(string $token, int $amount, string $paypalApiUrl);
+}
