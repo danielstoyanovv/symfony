29db7b9d65c14adb6d11ff7b4d64ab6360a99582
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-01-10 17:24:21 +0200

    Update Entity/Product.php - configure api platform

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,14 +2,26 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={
+            "get",
+ *          "put"
+ *     },
+ *     normalizationContext={"groups"={"product:read"}},
+ *     denormalizationContext={"groups"={"product:write"}}
+ * )
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  */
 class Product
@@ -25,32 +37,44 @@ class Product
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     min=4,
+     *     max=200,
+     *     maxMessage="Maxumum allowed length for name is: 200 characters."
+     * )
+     * @Groups({"product:read", "product:write", "file:read"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Groups({"product:read", "product:write", "file:read"})
      */
     private $description;
 
     /**
      * @ORM\Column(type="float", nullable=true)
+     * @Groups({"product:read", "product:write", "file:read"})
      */
     private $price;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Groups({"product:read", "product:write", "file:read"})
      */
     private $position;
 
     /**
      * @Gedmo\Slug(fields={"name"})
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"product:read", "file:read"})
      */
     private $slug;
 
     /**
-     * @ORM\OneToMany(targetEntity=File::class, mappedBy="product")
+     * @ORM\OneToMany(targetEntity=File::class, mappedBy="product", cascade={"persist"})
+     * @Groups({"product:read", "product:write", "file:write"})
      */
     private $image;
 
