7b5d9c79ccf74e1f246a751866e078003002c9d2
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-01-16 15:32:02 +0200

    Create LogoutListener - clear home page cache on logout

---
+++ src/Listeners/LogoutListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Cache\RedisManagerInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+use Symfony\Component\Security\Http\EventListener\SessionLogoutListener;
+
+class LogoutListener extends SessionLogoutListener
+{
+    /**
+     * @var RedisManagerInterface
+     */
+    private $redisManager;
+
+    public function __construct(
+        RedisManagerInterface $redisManager
+    ) {
+        $this->redisManager = $redisManager;
+    }
+
+    public function onLogout(LogoutEvent $event): void
+    {
+        $cache = $this->redisManager->getAdapter();
+        $cache->clear('home_page');
+        parent::onLogout($event);
+    }
+
+
+
+}
