7128515af8e1f124f8f6d3d72f5240c123f952e0
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-02-02 17:47:15 +0200

    Update vote_form.html.twig - define stimulus configuration - remove 2 input fields Update footer.html.twig Update menu.html.twig Update songs/index.html.twig configure stimulu Create song-vote_controller.js - create vote action Update webpack.config.js

---
+++ assets/controllers/song-vote_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    vote(e) {
+        let song = e.currentTarget.dataset.song;
+        let rating = $('#rating-song-' + song).val();
+        let voteAction = e.currentTarget.dataset.url;
+        let listAction =  e.currentTarget.dataset.list;
+        let queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+
+        if (urlParams.has('page')) {
+            listAction += "?page=" + urlParams.get('page');
+        }
+
+        if (rating && song && voteAction && listAction) {
+            $.ajax({
+                type: 'POST',
+                async: true,
+                url: voteAction,
+                data: { song: song, rating: rating },
+                success: function(result) {
+                    $.ajax({
+                        type: 'GET',
+                        url: listAction,
+                        success: function(result) {
+                            $('#song-vote-list').html(result);
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
