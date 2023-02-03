ceb5a4104cf37f0c0e81fcfce8b456b8bc61e738
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-02-03 17:38:57 +0200

    Remove unused js files Update song-vote_controller.js Update package.json - install some dependencies Update main layout templates update create_position.html.twig - configure stimulus Update template vote_form.html.twig Create new js controller product-sort_controller.js

---
+++ assets/controllers/product-sort_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from '@hotwired/stimulus';
+import { Sortable } from 'sortablejs';
+import $ from 'jquery';
+import {useDispatch} from "stimulus-use";
+
+export default class extends Controller {
+    connect() {
+        var el = document.getElementById('sortable');
+
+        if (el) {
+            let sortUrl =  this.element.dataset.url;
+            var sortable = Sortable.create(el, {
+                animation: 150,
+                onEnd: () => {
+                    $.ajax({
+                        type: 'POST',
+                        url: sortUrl,
+                        data: { products: sortable.toArray() },
+                        success: function(result) {
+                            location.reload();
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
